650e9a7622d6a1baaba23f8b3f32c7efb8846654
Sam Foster <sfoster@mozilla.com>
2019-04-27 16:28:46 -0700

    Fix the path for the requests destined for the python server/controller. Add uuid and name properties onto each game message

--- spectrum-client/htdocs/client.js
+++ spectrum-client/htdocs/client.js
@@ -5,9 +5,23 @@ var message_template = {
   'value': null,
 }
 
-var message_endpoint = 'client_update';
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+var message_endpoint = 'controller';
+var playerId = {
+  'north': uuidv4(),
+  'south': uuidv4()
+};
 
 function sendGameMessage(msg) {
+  msg.uuid = playerId[msg.position];
+  msg.name = msg.position[0].toUpperCase() + msg.position.substring(1);
+
   console.log("sendGameMessage:", msg);
   return fetch(message_endpoint, {
     method: "POST",
@@ -19,9 +33,11 @@ function sendGameMessage(msg) {
 
 function sendJoinMessage(btn) {
   let msg = Object.assign({}, message_template);
+  let position = btn.dataset.position.toLowerCase().trim();
   msg.action = 'join';
-  msg.position = btn.dataset.position;
+  msg.position = position;
   sendGameMessage(msg)
   .then(data => console.log("response: ", data))
   .catch(error => console.error(error));
 }
+
