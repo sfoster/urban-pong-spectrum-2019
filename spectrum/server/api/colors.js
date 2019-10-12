f7ac1619a47fb61136ed6cf0c2f110259ed677d1
Sam Foster <sfoster@mozilla.com>
2019-10-12 09:53:48 -0700

    Update colors message to use a north_color, south_color, result_color format

--- spectrum/server/api/colors.js
+++ spectrum/server/api/colors.js
@@ -5,7 +5,9 @@ module.exports = {
   sendColors(req, res, next) {
     let colorValues = req.body.colorValues;
     mqttClient.sendMessage("colors", {
-      colorValues
+      "north_color": colorValues[0],
+      "south_color": colorValues[1],
+      "result_color": colorValues[2],
     }).then(() => {
       res.json({ message: "colors sent" });
     }).catch(err => {
